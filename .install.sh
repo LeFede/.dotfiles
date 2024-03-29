036ab86dea1e482b37a40e8ad3bdcfb833a43c07
Federico Andres <lefedeok@gmail.com>
2024-03-29 11:40:17 -0300

    a

--- .install.sh
+++ .install.sh
@@ -10,23 +10,6 @@ fi
 apt update
 apt upgrade
 
-mkdir -p /home/fede/.local/bin
-chmod +x /home/fede/.local/bin
-mkdir -p /home/fede/.config
-chmod +x /home/fede/.config
-
-# Paso 3: Copiar .dotfiles/.config/nvim a ~/.config/
-cp -r /home/fede/.dotfiles/.config/nvim /home/fede/.config/
-
-# Paso 4: Copiar .dotfiles/.config/.snippets a ~/.config/
-cp -r /home/fede/.dotfiles/.config/.snippets /home/fede/.config/
-
-# Paso 5: Copiar .dotfiles/.local/bin/nvim a ~/.local/bin/
-cp -r /home/fede/.dotfiles/.local/bin/nvim.appimage /home/fede/.local/bin/
-
-# Paso 6: Borrar ~/.bashrc y copiar .config/.bashrc a ~/
-rm -f /home/fede/.bashrc
-cp /home/fede/.dotfiles/.bashrc /home/fede/
 
 # Fondos
 # sudo apt-get install -y nitrogen
