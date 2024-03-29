b7c37934e2c69d596efa0dccb0190e018712d2c2
Federico Andres <lefedeok@gmail.com>
2024-03-29 09:34:01 -0300

    snipets install

--- .config/install.sh
+++ .config/install.sh
@@ -17,13 +17,20 @@ apt upgrade
 # sudo apt-get install -y maim xclip 
 
 # DEV
-# sudo apt-get install -y ripgrep clangd git zip unzip python3-pip build-essential wget fuse curl
+sudo apt-get install -y ripgrep clangd git zip unzip python3-pip build-essential wget fuse curl
+
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# github
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 
 # wget "https://discord.com/api/download?platform=linux&format=deb" - O discord.deb
 # sudo apt install -y ./discord.deb
-
-# curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-# sudo apt install -y nodejs
 
 # audio
 # sudo apt-get install -y expat libxml2-dev pkg-config libasound2-dev libssl-dev cmake libfreetype6-dev libexpat1-dev libxcb-composite0-dev libharfbuzz-dev 
