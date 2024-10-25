5c305217aaf6c55451adfc708d1aa558a61e3dfe
Federico Andres <lefedeok@gmail.com>
2024-10-25 10:23:09 -0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -91,5 +91,6 @@ curl -fsSL https://bun.sh/install | bash
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install libgl1:i386 libdrm2:i386
+sudo apt install libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-libs-amd64:amd64 steam-libs-i386:i386 xdg-desktop-portal xdg-desktop-portal-gtk
 
 echo "Buenisimo loco 👍️"
